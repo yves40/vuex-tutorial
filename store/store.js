1a38e54b2686d024d5ccd69b1c637d8ca42c626f
yves40 <yves.toubhans@free.fr>
2018-12-13 17:28:12 +0100

    Decrement ops now similar to increment

--- store/store.js
+++ store/store.js
@@ -3,7 +3,7 @@
 
     Dec 09 2018   Initial
     Dec 10 2018   Learn Getters, Actions, Mutations...
-    Dec 11 2018   Track requests in a log window
+    Dec 11 2018   Track requests in a log window. Learn Promises
     Dec 12 2018   Remove finished request from the log window
     Dec 13 2018   Modify counter management
 ----------------------------------------------------------------------------*/
@@ -15,13 +15,13 @@ Vue.use(Vuex);
 const state = {
     reqid: 10000,
     count: 5,
-    Version: 'store, 1.43 Dec 13 2018',
+    Version: 'store, 1.45 Dec 13 2018',
     message: '',
     mutationrunning: 0, // Used to track the current number of operations running
     requests: [],
-    MAXRUN: 3, // Max number of concurrent operations
-    MINDELAY: 5,
-    MAXDELAY: 20,
+    MAXRUN: 4, // Max number of concurrent operations
+    MINDELAY: 3,
+    MAXDELAY: 12,
 };
 
 const getters = {
@@ -51,54 +51,54 @@ function generateRandomNumber(min , max)
 
 const mutations = { // Synchronous
     increment(state) {
-            ++state.mutationrunning;
-            let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
-            state.message = 'Increment requested, should take ' + taskduration +  ' seconds'; 
-            let thereqid = ++state.reqid;
-            state.requests.push( { date: new Date().toString(), label: 'Increment OPS for ' + taskduration + ' sec', id: thereqid });
-            let sometasktakingtime = new Promise(function(resolve, reject) {
-                setTimeout(function() {
-                    if ( state.count < 10 ){
-                        state.count++;
-                        resolve('Increment done : now removing REQID ' + thereqid);
-                    }
-                    else{
-                        resolve('Increment aborted, max limit reached : now removing REQID ' + thereqid);
-                    }
-                    let filtered = state.requests.filter( function(value, index){
-                        return value.id != thereqid;
-                    })
-                    state.requests = filtered;
-                    --state.mutationrunning
-                }, taskduration * 1000)})
-            .then(function(message) {
-                state.message = message;
-            });
+        ++state.mutationrunning;
+        let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
+        state.message = 'Increment requested, should take ' + taskduration +  ' seconds'; 
+        let thereqid = ++state.reqid;
+        state.requests.push( { date: new Date().toString(), label: 'Increment OPS for ' + taskduration + ' sec', id: thereqid });
+        let sometasktakingtime = new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                if ( state.count < 10 ){
+                    state.count++;
+                    resolve('Increment done : now removing REQID ' + thereqid);
+                }
+                else{
+                    resolve('Increment aborted, max limit reached : now removing REQID ' + thereqid);
+                }
+                let filtered = state.requests.filter( function(value, index){
+                    return value.id != thereqid;
+                })
+                state.requests = filtered;
+                --state.mutationrunning
+            }, taskduration * 1000)})
+        .then(function(message) {
+            state.message = message;
+        });
     },
     decrement(state) {
-        if (state.count > 0 ) {
-            state.count--;
-            ++state.mutationrunning;
-            let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
-            state.message = 'Decrement requested, should take ' + taskduration +  ' seconds';
-            let thereqid = ++state.reqid;
-            state.requests.push( { date: new Date().toString(), label: 'Decrement OPS for ' + taskduration + ' sec', id: thereqid });
-            let sometasktakingtime = new Promise(function(resolve, reject) {
-                setTimeout(function() {
+        ++state.mutationrunning;
+        let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
+        state.message = 'Decrement requested, should take ' + taskduration +  ' seconds';
+        let thereqid = ++state.reqid;
+        state.requests.push( { date: new Date().toString(), label: 'Decrement OPS for ' + taskduration + ' sec', id: thereqid });
+        let sometasktakingtime = new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                if (state.count > 0 ) {
+                    state.count--;
                     resolve('Decrement done : now removing REQID ' + thereqid);
-                    let filtered = state.requests.filter( function(value, index){
-                        return value.id != thereqid;
-                    })
-                    state.requests = filtered;
-                    --state.mutationrunning
-                }, taskduration * 1000)})
-            .then(function(message) {
-                state.message = message;
-            });
-        }
-        else {
-            state.message = 'Already 0'
-        }
+                }
+                else {
+                    resolve('Decrement aborted, already 0 : now removing REQID ' + thereqid);
+                }
+                let filtered = state.requests.filter( function(value, index){
+                    return value.id != thereqid;
+                })
+                state.requests = filtered;
+                --state.mutationrunning
+            }, taskduration * 1000)})
+        .then(function(message) {
+            state.message = message;
+        });
     },
 };
 
