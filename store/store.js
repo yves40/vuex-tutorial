5f60c2b8f5d59965d2548f58b8a4f2fb3d92a972
yves40 <yves.toubhans@free.fr>
2018-12-14 19:18:15 +0100

    Work on log and requests messages. Date format, ID, etc.

--- store/store.js
+++ store/store.js
@@ -6,24 +6,26 @@
     Dec 11 2018   Track requests in a log window. Learn Promises
     Dec 12 2018   Remove finished request from the log window
     Dec 13 2018   Modify counter management
+    Dec 14 2018   MAxlog and scroll window
 ----------------------------------------------------------------------------*/
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { request } from 'http';
 
 Vue.use(Vuex);
 
 const state = {
     reqid: 10000,
     count: 5,
-    Version: 'store, 1.49 Dec 13 2018',
+    Version: 'store, 1.63 Dec 15 2018',
     logs: [],
     mutationrunning: 0, // Used to track the current number of operations running
-    requests: [],
+    requests: [], // Running requests
     MAXRUN: 4, // Max number of concurrent operations
-    MINDELAY: 3,
-    MAXDELAY: 12,
+    MINDELAY: 8,
+    MAXDELAY: 16,
+    MAXLOG:8,
 };
-
 const getters = {
     fetchCount(state) {
         return state.count;
@@ -43,33 +45,44 @@ const getters = {
 
 };
 
-// Simulate some long task running between 5 and 20 sec
+// ---------------------------------------------------- Simulate some long task running between 5 and 20 sec
 function generateRandomNumber(min , max) 
 {
     return Math.floor(Math.random() * (max-min) + min) ;
 }
-// Logger
-function log(mess) {
-    let timestamp = new Date().toString();
-    let x = timestamp.indexOf('GMT');
-    state.logs.push({ date: timestamp.substring(0, x - 1), message: mess });
+// ---------------------------------------------------- Logger
+const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+function log(mess, id) {
+    let d = new Date();
+    if (state.logs.length === state.MAXLOG) {
+        state.logs.shift();
+    }
+    state.logs.push({ date: months[d.getMonth()] + '-' + d.getDate() + '-' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() , message: id + ' ' + mess });
 }
 
+// ---------------------------------------------------- New request in Queue
+function request(reqid) {
+    let td = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
+    state.requests.push( { date: new Date().toString(), label: 'Increment OPS for ' + td + ' sec', id: reqid });
+    return td;
+}
+
+// ---------------------------------------------------- VUEX mutations
 const mutations = { // Synchronous
     increment(state) {
         ++state.mutationrunning;
-        let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
-        log('Increment requested, should take ' + taskduration +  ' seconds'); 
-        let thereqid = ++state.reqid;
-        state.requests.push( { date: new Date().toString(), label: 'Increment OPS for ' + taskduration + ' sec', id: thereqid });
+        let thereqid = state.reqid;
+        let taskduration = request(thereqid);
+        log('Increment requested, should take ' + taskduration +  ' seconds', thereqid);
+        ++state.reqid;
         let sometasktakingtime = new Promise(function(resolve, reject) {
             setTimeout(function() {
                 if ( state.count < 10 ){
                     state.count++;
-                    resolve('Increment done : now removing REQID ' + thereqid);
+                    resolve('Increment done');
                 }
                 else{
-                    resolve('Increment aborted, max limit reached : now removing REQID ' + thereqid);
+                    resolve('Increment aborted, max limit reached');
                 }
                 let filtered = state.requests.filter( function(value, index){
                     return value.id != thereqid;
@@ -78,23 +91,23 @@ const mutations = { // Synchronous
                 --state.mutationrunning
             }, taskduration * 1000)})
         .then(function(message) {
-            log(message);
+            log(message, thereqid);
         });
     },
     decrement(state) {
         ++state.mutationrunning;
-        let taskduration = generateRandomNumber(state.MINDELAY, state.MAXDELAY);
-        log('Decrement requested, should take ' + taskduration +  ' seconds');
-        let thereqid = ++state.reqid;
-        state.requests.push( { date: new Date().toString(), label: 'Decrement OPS for ' + taskduration + ' sec', id: thereqid });
+        let thereqid = state.reqid;
+        let taskduration = request(thereqid);
+        log('Decrement requested, should take ' + taskduration +  ' seconds', thereqid);  
+        ++state.reqid;
         let sometasktakingtime = new Promise(function(resolve, reject) {
             setTimeout(function() {
                 if (state.count > 0 ) {
                     state.count--;
-                    resolve('Decrement done : now removing REQID ' + thereqid);
+                    resolve('Decrement done');
                 }
                 else {
-                    resolve('Decrement aborted, already 0 : now removing REQID ' + thereqid);
+                    resolve('Decrement aborted, already 0');
                 }
                 let filtered = state.requests.filter( function(value, index){
                     return value.id != thereqid;
@@ -103,11 +116,12 @@ const mutations = { // Synchronous
                 --state.mutationrunning
             }, taskduration * 1000)})
         .then(function(message) {
-            log(message );
+            log(message, thereqid );
         });
     },
 };
 
+// ---------------------------------------------------- VUEX actions
 const actions = { // Asynchronous
     increment(context) { 
         context.commit('increment'); 
