ebace7519e7bb49b13dbe45551bf2d5644ca200e
yves40 <yves.toubhans@free.fr>
2018-12-21 00:17:19 +0100

    Got the data after URL call !

--- urltest.js
+++ urltest.js
@@ -4,7 +4,7 @@
 //    Dec 20 2018    Initial
 //----------------------------------------------------------------------------
 
-const Version = "urltest.js, Dec 20 2018, 1.20 ";
+const Version = "urltest.js, Dec 20 2018, 1.28 ";
 
 const fetch = require("node-fetch");
 
@@ -12,7 +12,6 @@ console.log('----------------- urltest -------------------------------');
 console.log(Version);
 const camuser = process.env.camuser;
 const campass = process.env.campass;
-let done = false;
 
 if (camuser === undefined) {
   console.log('Please set camuser variable in your shell');
@@ -26,30 +25,22 @@ if (campass === undefined) {
 // ------------ The working function ------------------------------------------
 function urlcall(theurl) {
   console.log('Calling ---------- ' + theurl);
+  // Look @ : https://appdividend.com/2018/08/20/javascript-fetch-api-example-tutorial/
   let result = fetch(theurl)
-  .then(response => console.log(response))
-  .then(data =>
-    console.log(data === undefined ? 'No data' : data)
-  )
+  .then(response => { 
+      return response.text();
+  })
+  .then(text => {
+      console.log(text === undefined ? 'No data' : text);
+  })
   .catch(error => {
+    console.log('************************************* E R R O R ********************************');
     console.log(error);
-    done = true;
   });
-
-/*
-  .then(data => { 
-    console.log(data.json());
-    done = true;
-  })
-  .then(res => { 
-    console.log(res) 
-    done = true;
-  })
-  */
 };
 
 // ------------ Call Test URL ------ ------------------------------------------
-const cam1 = 'http://82.229.126.186:8080/cgi-bin/CGIProxy.fcgi?cmd=getIPInfo';
+const cam1 = process.env.camurl || 'http://82.229.126.186:8080/cgi-bin/CGIProxy.fcgi?cmd=getIPInfo';
 const target = cam1 + '&usr=' + camuser + '&pwd=' + campass; 
 console.log('Calling CAM1 now :' + cam1);
 urlcall(target);
