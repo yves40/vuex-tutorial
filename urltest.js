29a30340e661177fc97b69562afd6e5a552daf4e
yves40 <yves.toubhans@free.fr>
2018-12-21 19:03:51 +0100

    Test URLs and environment variables

--- urltest.js
+++ urltest.js
@@ -2,9 +2,10 @@
 //    urltest.js
 //
 //    Dec 20 2018    Initial
+//    Dec 21 2018    WIP on test URLs
 //----------------------------------------------------------------------------
 
-const Version = "urltest.js, Dec 20 2018, 1.28 ";
+const Version = "urltest.js, Dec 21 2018, 1.30 ";
 
 const fetch = require("node-fetch");
 
@@ -13,19 +14,13 @@ console.log(Version);
 const camuser = process.env.camuser;
 const campass = process.env.campass;
 
-if (camuser === undefined) {
-  console.log('Please set camuser variable in your shell');
-  process.exit(1);
-}
-if (campass === undefined) {
-  console.log('Please set campass variable in your shell');
-  process.exit(1);
-}
-
 // ------------ The working function ------------------------------------------
 function urlcall(theurl) {
   console.log('Calling ---------- ' + theurl);
+
   // Look @ : https://appdividend.com/2018/08/20/javascript-fetch-api-example-tutorial/
+  //      @ : https://javascript.info/promise-chaining 
+  
   let result = fetch(theurl)
   .then(response => { 
       return response.text();
@@ -40,7 +35,13 @@ function urlcall(theurl) {
 };
 
 // ------------ Call Test URL ------ ------------------------------------------
-const cam1 = process.env.camurl || 'http://82.229.126.186:8080/cgi-bin/CGIProxy.fcgi?cmd=getIPInfo';
-const target = cam1 + '&usr=' + camuser + '&pwd=' + campass; 
-console.log('Calling CAM1 now :' + cam1);
+const cam1 = process.env.camurl || 'https://jsonplaceholder.typicode.com/users/2';
+let target = '';
+if (camuser !== undefined && campass !== undefined) {
+  target = cam1 + '&usr=' + camuser + '&pwd=' + campass; 
+}
+else {
+  target = cam1; 
+}
+console.log('Calling CAM1 now :' + target);
 urlcall(target);
