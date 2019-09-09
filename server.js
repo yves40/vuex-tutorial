1c6b433feb2790b6e6af8bf1c5b5203792af73c9
yves40 <yves.toubhans@free.fr>
2019-09-09 11:34:39 +0200

    and a few other things

--- server.js
+++ server.js
@@ -2,22 +2,54 @@
 //    server.js
 //
 //    Dec 18 2018    Initial
+//    Sep 08 2019    Wake up old code
 //----------------------------------------------------------------------------
 
-const Version = "server.js, Dec 18 2018, 1.05 ";
+const Version = "server.js, Sep 08 2019, 1.11 ";
 
 const express = require('express');
+const cors = require('cors');
+const axiosutility = require("./src/utilities/axiosutility");
+const logger = require("./src/utilities/logger");
+const corsutility = require("./src/utilities/corsutility");
+const bodyParser  = require('body-parser');
 
+//----------------------------------------------------------------------------
+// axiosutility test
+//----------------------------------------------------------------------------
+logger.info("---------------------------------------------------------");
+logger.info('AXIOS :');
+logger.info("---------------------------------------------------------");
+logger.info('Using axiosutility: ' + axiosutility.getVersion());
+
+
+//----------------------------------------------------------------------------
+// Cross-Origin Resource Sharing
+// https://github.com/expressjs/cors/blob/master/README.md
+//----------------------------------------------------------------------------
+logger.info("---------------------------------------------------------");
+logger.info('CORS Security setting, sites list:');
+logger.info("---------------------------------------------------------");
+
+let loop = 0;
+let sitelist = corsutility.getCORSwhitelist();
+for (; loop < sitelist.length; ++loop) {
+  logger.info('Site : ' + sitelist[loop]);
+}
 
 const app = express();
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/dist'));
 const router = express.Router();
 
-console.log('----------------- Set some routes -------------------------------');
+logger.info('----------------- Set some routes -------------------------------');
 // Home URL
-console.log('/');
+logger.info('/');
 router.get('/', function (req, res) {
   res.json({message: 'API initialized'});
+})
+router.get('/test', function (req, res) {
+  res.json({message: 'API test is OK for me but CORS blocks Chrome'});
 })
 
 // For the favicon boring request error
@@ -29,10 +61,12 @@ app.use( function(req, res, next) {
   return next();
 });
 
-console.log('----------------- Server status -------------------------------');
-const port = process.env.API_PORT || 8080;
+app.use(cors(corsutility.getCORS()));
+
+logger.info('----------------- Server status -------------------------------');
+const port = process.env.API_PORT || 8088;
 app.use('/', router);
 app.listen(port, function() {
-    console.log(Version);
-    console.log(`Vuex server started on ${port}`);
+  logger.info(Version);
+  logger.info(`Vuex server started on ${port}`);
 });
