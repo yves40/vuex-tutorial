e075086ec71d7433aee14136da965611652737fd
yves40 <yves.toubhans@free.fr>
2019-09-10 10:40:31 +0200

    Moved store file

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import { store } from '../store/store'
+import { store } from './store/store'
 
 Vue.config.productionTip = false
 
